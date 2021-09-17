ef381703505e79c2166faa39779c359047e11888
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2021-09-17 03:46:22 -0400

    Update create-keystore-and-sign.sh

--- create-keystore-and-sign.sh
+++ create-keystore-and-sign.sh
@@ -5,7 +5,7 @@ npm -g install randomstring'
 randomstring
 rm -rf my-release-key.jks
 keytool -genkey -v -keystore my-release-key.jks -alias alias_name -keyalg RSA -keysize 2048 -validity 10000
-platforms/android/app/build/outputs/apk/debug/app-debug.apk .
+cp -rf platforms/android/app/build/outputs/apk/debug/app-debug.apk .
 echo "replace this line for /home/Android/Sdk where your android/android-sdk command line sdk is"
 /home/Android/Sdk/build-tools/29.0.3/apksigner sign --ks my-release-key.jks --out website-app-Android-signed.apk app-debug.apk
  

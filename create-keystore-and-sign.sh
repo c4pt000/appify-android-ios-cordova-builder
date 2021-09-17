aea5da5762eeb797a63a6ef70219fc4a6e7c0a03
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2021-09-17 03:30:16 -0400

    Update create-keystore-and-sign.sh

--- create-keystore-and-sign.sh
+++ create-keystore-and-sign.sh
@@ -1,10 +1,11 @@
-echo "requires running java11"
+echo "requires running java11 from cordova project folder root where platforms is in ls -l"
 echo ' 
 yum install nodejs -y
 npm -g install randomstring'
 randomstring
 rm -rf my-release-key.jks
 keytool -genkey -v -keystore my-release-key.jks -alias alias_name -keyalg RSA -keysize 2048 -validity 10000
-cp -rf wallet/build/outputs/apk/prod/release/wallet-prod-release-unsigned.apk .
-/home/Android/Sdk/build-tools/29.0.3/apksigner sign --ks my-release-key.jks --out RadioCoin-Android-signed.apk wallet-prod-release-unsigned.apk
+platforms/android/app/build/outputs/apk/debug/app-debug.apk .
+echo "replace this line for /home/Android/Sdk where your android/android-sdk command line sdk is"
+/home/Android/Sdk/build-tools/29.0.3/apksigner sign --ks my-release-key.jks --out RadioCoin-Android-signed.apk app-debug.apk
  

9206afe718041d7716f7033592b995660aa6f0c9
Your Name <you@example.com>
2022-01-06 13:12:48 -0500

    build new switchable scripts for www/index.html https or http

---
+++ build-ANDROID-HTTPS.sh
@@ -0,0 +1,23 @@
+java8-switch-on
+echo "requires build-tools 31.0.0"
+echo 'sdkmanager "build-tools;31.0.0"'
+echo ""
+echo ""
+echo ""
+echo "this is a HTTPS build script if your site in www/index.html is HTTP hit crtl-C to cancel script will pause for 5 seconds"
+sleep 5s
+cp -rf ./platforms/android/app/src/main/AndroidManifest.https.xml ./platforms/android/app/src/main/AndroidManifest.xml
+
+echo ' for problems where a file in the 31.0.0 build-tools might get corrupt with this script 
+
+replace /root/Android/Sdk
+with your actual location of Android-SDK
+
+mv /root/Android/Sdk/build-tools/31.0.0/d8 /root/Android/Sdk/build-tools/31.0.0/dx
+mv /root/Android/Sdk/build-tools/31.0.0/lib/d8.jar /root/Android/Sdk/build-tools/31.0.0/lib/dx.jar
+'
+sleep 10s
+ cordova plugin add cordova-plugin-inappbrowser
+npm install
+export GRADLE_OPTS="-Dorg.gradle.parallel=true -Dorg.gradle.workers.max=8 -Dorg.gradle.daemon=true -Dorg.gradle.configureondemand=true"
+cordova build android --verbose

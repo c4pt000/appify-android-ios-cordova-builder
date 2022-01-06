f0b5d75da8548c97ef8aed88e0fdc3f8852376a7
Your Name <you@example.com>
2022-01-06 12:21:04 -0500

    changes to build-app (android apk) script

--- build-app.sh
+++ build-app.sh
@@ -4,6 +4,13 @@ echo 'sdkmanager "build-tools;31.0.0"'
 echo ""
 echo ""
 echo ""
+ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+nvm install 14.18
+nvm use 14.18
+nvm alias default 14.18
+node -v
+sleep 1s
+npm install 
 echo ' for problems where a file in the 31.0.0 build-tools might get corrupt with this script 
 
 replace /root/Android/Sdk
